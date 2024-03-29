fb650c29ff09bb1a17423392084e9bd9951dec73
Ahsans-GitHub <ahsanshahid5471.com>
2024-03-29 17:22:33 +0500

    tweak the test and added console at the end

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-  { message: "Enter your first number", type: "number", name: "firstNumber" },
-  { message: "Enter your second number", type: "number", name: "secondNumber" },
+  { message: "Enter  first number", type: "number", name: "firstNumber" },
+  { message: "Enter  second number", type: "number", name: "secondNumber" },
   {
     message: "Select one of the operators to perform operation",
     type: "list",
@@ -25,3 +25,5 @@ if (answer.operator === "Multiplication")
 
 if (answer.operator === "Division")
   console.log(answer.firstNumber / answer.secondNumber);
+
+  console.log("The End")
